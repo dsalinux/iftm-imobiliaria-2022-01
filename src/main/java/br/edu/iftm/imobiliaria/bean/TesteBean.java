49ab25ec0104e22d98770a4ec38959f70ad727d3
Danilo Souza Almeida <dsalinux@gmail.com>
2022-04-22 08:34:54 -0300

    Melhorias na Interface - Ajax Status.

--- src/main/java/br/edu/iftm/imobiliaria/bean/TesteBean.java
+++ src/main/java/br/edu/iftm/imobiliaria/bean/TesteBean.java
@@ -2,6 +2,7 @@ package br.edu.iftm.imobiliaria.bean;
 
 import br.edu.iftm.imobiliaria.entity.Usuario;
 import java.io.Serializable;
+import java.util.Date;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -14,6 +15,10 @@ public class TesteBean implements Serializable{
     private String nome;
     
     public void dizerOla(){
+        long terminarEm = new Date().getTime()+2000;
+        while(terminarEm > new Date().getTime()){
+            
+        }
 //        FacesContext fc = FacesContext.getCurrentInstance();
 //        Usuario usuario = new Usuario();
 //        usuario.setNome(nome);
