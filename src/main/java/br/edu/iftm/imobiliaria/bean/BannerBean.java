92daed705d0af85de2b1c6be2b4dd9ba00e06eb6
Danilo Souza Almeida <dsalinux@gmail.com>
2022-06-21 20:42:48 -0300

    Conclusão do componentente Banco de Imagens.

--- src/main/java/br/edu/iftm/imobiliaria/bean/BannerBean.java
+++ src/main/java/br/edu/iftm/imobiliaria/bean/BannerBean.java
@@ -2,10 +2,8 @@ package br.edu.iftm.imobiliaria.bean;
 
 import br.edu.iftm.imobiliaria.entity.Banner;
 import br.edu.iftm.imobiliaria.entity.Imagem;
-import br.edu.iftm.imobiliaria.interceptors.anotation.Transacao;
 import br.edu.iftm.imobiliaria.logic.BannerLogic;
 import br.edu.iftm.imobiliaria.logic.ImagemLogic;
-import br.edu.iftm.imobiliaria.util.HashUtil;
 import br.edu.iftm.imobiliaria.util.exception.ErroNegocioException;
 import br.edu.iftm.imobiliaria.util.exception.ErroSistemaException;
 import java.util.ArrayList;
@@ -13,14 +11,11 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 import org.primefaces.model.file.UploadedFile;
-import org.primefaces.shaded.commons.io.FilenameUtils;
 
 @Named
 @SessionScoped
@@ -34,12 +29,6 @@ public class BannerBean extends CrudBean<Banner, BannerLogic> {
     @Getter
     @Setter
     private UploadedFile file;
-
-    private enum Estado {
-        BUSCAR,//Default
-        CRIAR,
-        EDITAR
-    }
 
     public BannerBean() {
         super(Banner.class);
@@ -60,26 +49,6 @@ public class BannerBean extends CrudBean<Banner, BannerLogic> {
             Logger.getLogger(UsuarioBean.class.getName()).log(Level.SEVERE, null, ex);
         }
         return new ArrayList<>();
-    }
-
-    @Override
-    @Transacao
-    public void salvar() {
-        try {
-            Imagem imagem = imagemLogic.uploadToSystem(file);
-            this.getEntidade().setImagem(imagem);
-            logic.salvar(this.getEntidade());
-            addInfo("Salvo com sucesso.");
-            // this.estado = Estado.BUSCAR;
-            //Não consigo sei a melhor maneira para alterar o estado
-        } catch (ErroNegocioException ex) {
-            addAviso(ex);
-        } catch (ErroSistemaException ex) {
-            addErro(ex);
-            Logger.getLogger(CrudBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (Exception ex) {
-            Logger.getLogger(BannerBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 
 }
